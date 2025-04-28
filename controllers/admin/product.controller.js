17a4690e20aa1d503a03da0445a12621f83e13ae
Bangdx1412 <doxuanbang14122005@gmail.com>
2025-04-29 00:06:46 +0700

    Đổ dữ liệu ra trang list products

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,7 +1,17 @@
-// [GET] /admin/products
+// Nhúng model vào để sử dụng
+const Product = require("../../models/product.model")
 
-module.exports.listProduct = (req,res)=>{
+// [GET] /admin/products
+// hàm để lấy ra tất cả list products
+module.exports.listProduct = async(req,res)=>{
+    // lấy dữ liệu từ database
+    const products = await Product.find({
+        deleted: false
+    })
+    console.log(products);
+    
     res.render('admin/pages/products/index',{
-        pageTitle:"Trang list product"
+        pageTitle:"Trang list product",
+        products: products
     })
 }

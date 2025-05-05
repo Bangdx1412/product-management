311c2056b74824cba4529fed787b51613ab72d04
Bangdx1412 <doxuanbang14122005@gmail.com>
2025-05-05 16:16:13 +0700

    Thực hiện kết nối mongdb online

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports.connect = async ()=>{
     try {
       await  mongoose.connect(process.env.MONGO_URL);
-    console.log("Ket noi thanh cong");
+      console.log("Ket noi thanh cong");
     
     } catch (error) {
         console.log("Ket noi that bai");

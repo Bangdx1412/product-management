9d187d35503b0ffe48b2bc0a0ce778cab2c321c9
Bangdx1412 <doxuanbang14122005@gmail.com>
2025-05-05 15:25:19 +0700

    Trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -7,17 +7,32 @@ module.exports.index = async (req,res)=>{
     const products = await Product.find({
         status:"active",
         deleted:"false"
-    })
+    }).sort({ position: "desc" })
     const productNew = products.map(item=>{
         item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(0)
         return item
     })
-   
-    
-    console.log(productNew);
-    
     res.render('client/pages/products/index',{
         pageTitle: "Trang danh sach san pham",
         products: productNew
     })
+}
+module.exports.detail = async (req,res)=>{
+  
+    try {
+        const find = {
+          deleted: false,
+          slug: req.params.slug,
+          status: "active"
+        };
+        const product = await Product.findOne(find);
+    
+        res.render("client/pages/products/detail", {
+          pageTitle: product.title,
+          product: product,
+        });
+      } catch (error) {
+        res.redirect(`/products`);
+      }
+
 }

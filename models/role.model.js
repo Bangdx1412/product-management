61dfe628448529e768de63f096e3eb6d4189d764
Bangdx1412 <doxuanbang14122005@gmail.com>
2025-05-08 23:45:05 +0700

    Tạo colection roles and role.model

---
+++ models/role.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+mongoose.plugin(slug);
+const roleSchema = new mongoose.Schema(
+  {
+    title: String, //Ví dụ tiêu đề là: Sản phẩm 1
+    description: String,
+    permissions:{ //nhóm quyền
+        type: Array,
+        default:[]
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    // Thêm createdAt , updatedAt đây là thuộc tính có sẵn của mongoose mục Timestamps
+    timestamps: true,
+  }
+);
+const Role = mongoose.model("Role", roleSchema, "roles");
+
+module.exports = Role;

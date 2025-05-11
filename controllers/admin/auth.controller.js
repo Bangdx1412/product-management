f3de5aac6f9a12172973766a238d88ed57440ff3
Bangdx1412 <doxuanbang14122005@gmail.com>
2025-05-11 16:03:39 +0700

    Thêm middleware cho router

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -3,41 +3,45 @@ const Account = require("../../models/account.model");
 const flash = require("express-flash");
 const systemConfig = require("../../config/system");
 // [GET] /admin/auth/login
-module.exports.login = (req,res)=>{
-    res.render('admin/pages/auth/login',{
-        pageTitle: "Đăng nhập"
-    })
-}
-module.exports.loginPost = async (req,res)=>{
-    const email = req.body.email;
-    const password = req.body.password;
-    
-    const user = await Account.findOne({
-        email:email,
-        deleted:false
-    })
-    if(!user){
-        req.flash("errors","Email không tồn tại!!!")
-        res.redirect(req.get("Referrer") || "/");
-        return;
-    }
-    if(md5(password) !== user.password){
-        req.flash("errors","Sai mật khẩu!!!")
-        res.redirect(req.get("Referrer") || "/");
-        return;
-    }
-    
-    if(user.status == "inactive"){
-        req.flash("errors","Tài khoản đã bị khóa!!!")
-        res.redirect(req.get("Referrer") || "/");
-        return;
-    }
-    res.cookie("token",user.token);
+module.exports.login = (req, res) => {
+  if (req.cookies.token) {
     res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
-}
-module.exports.logout = (req,res)=>{
-    // Xóa token trong cookie 
-    res.clearCookie("token")
-    req.flash("success","Đăng xuất thành công")
-    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
-}
+  } else {
+    res.render("admin/pages/auth/login", {
+      pageTitle: "Đăng nhập",
+    });
+  }
+};
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await Account.findOne({
+    email: email,
+    deleted: false,
+  });
+  if (!user) {
+    req.flash("errors", "Email không tồn tại!!!");
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+  if (md5(password) !== user.password) {
+    req.flash("errors", "Sai mật khẩu!!!");
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+
+  if (user.status == "inactive") {
+    req.flash("errors", "Tài khoản đã bị khóa!!!");
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+  res.cookie("token", user.token);
+  res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+};
+module.exports.logout = (req, res) => {
+  // Xóa token trong cookie
+  res.clearCookie("token");
+  req.flash("success", "Đăng xuất thành công");
+  res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+};
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,21 +1,25 @@
-const systemConfig = require("../../config/system")
+const systemConfig = require("../../config/system");
 
+const authMiddleware = require("../../middlewares/admin/auth.middleware");
 // Nhúng route
-const dashboardRoutes = require("./dashboard.route")
-const productRoutes = require("./product.route")
-const categoryRoutes = require("./products-category.route")
-const roleRoutes = require("./role.route")
-const accountRoutes = require("./account.route")
-const authRoutes = require("./auth.route")
+const dashboardRoutes = require("./dashboard.route");
+const productRoutes = require("./product.route");
+const categoryRoutes = require("./products-category.route");
+const roleRoutes = require("./role.route");
+const accountRoutes = require("./account.route");
+const authRoutes = require("./auth.route");
 
-module.exports = (app)=>{
-    const PATH_ADMIN = systemConfig.prefixAdmin
-    // Route
-    app.use(PATH_ADMIN + '/dashboard', dashboardRoutes)
-    app.use(PATH_ADMIN + '/products', productRoutes)
-    app.use(PATH_ADMIN + '/products-category', categoryRoutes)
-    app.use(PATH_ADMIN + '/roles', roleRoutes)
-    app.use(PATH_ADMIN + '/accounts', accountRoutes)
-    app.use(PATH_ADMIN + '/auth', authRoutes)
-
-}
+module.exports = (app) => {
+  const PATH_ADMIN = systemConfig.prefixAdmin;
+  // Route
+  app.use(
+    PATH_ADMIN + "/dashboard",
+    authMiddleware.requireAuth,
+    dashboardRoutes
+  );
+  app.use(PATH_ADMIN + "/products",authMiddleware.requireAuth, productRoutes);
+  app.use(PATH_ADMIN + "/products-category",authMiddleware.requireAuth, categoryRoutes);
+  app.use(PATH_ADMIN + "/roles",authMiddleware.requireAuth, roleRoutes);
+  app.use(PATH_ADMIN + "/accounts",authMiddleware.requireAuth, accountRoutes);
+  app.use(PATH_ADMIN + "/auth", authRoutes);
+};

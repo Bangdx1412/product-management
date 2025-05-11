aa69eec58df318235c40987b2090b0b352f72191
Bangdx1412 <doxuanbang14122005@gmail.com>
2025-05-12 00:38:17 +0700

    Lấy danh sách theo danh mục sản phẩm

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,40 +1,57 @@
-
-
-const Product = require('../../models/product.model');
-const productsHelper = require("../../helpers/products")
+const ProductCategory = require("../../models/products-category.model");
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
+const productCategoryHelper = require("../../helpers/product-category");
 // [GET] /products
-module.exports.index = async (req,res)=>{
-    // Get all products
+module.exports.index = async (req, res) => {
+  // Get all products
   try {
     const products = await Product.find({
-      status:"active",
-      deleted:false
-  })
-  const productNew = productsHelper.priceNewProducts(products)
-  res.render('client/pages/products/index',{
+      status: "active",
+      deleted: false,
+    });
+    const productNew = productsHelper.priceNewProducts(products);
+    res.render("client/pages/products/index", {
       pageTitle: "Trang danh sach san pham",
-      products: productNew
-  })
+      products: productNew,
+    });
   } catch (error) {
     res.redirect(`/`);
   }
-}
-module.exports.detail = async (req,res)=>{
-  
-    try {
-        const find = {
-          deleted: false,
-          slug: req.params.slug,
-          status: "active"
-        };
-        const product = await Product.findOne(find);
-    
-        res.render("client/pages/products/detail", {
-          pageTitle: product.title,
-          product: product,
-        });
-      } catch (error) {
-        res.redirect(`/products`);
-      }
+};
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      slug: req.params.slug,
+      status: "active",
+    };
+    const product = await Product.findOne(find);
 
-}
+    res.render("client/pages/products/detail", {
+      pageTitle: product.title,
+      product: product,
+    });
+  } catch (error) {
+    res.redirect(`/products`);
+  }
+};
+module.exports.category = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    status:"active",
+    deleted: false,
+  });
+  
+  const listSubCategory = await productCategoryHelper.getSubCategory(category.id);
+  const listSubCategoryId = listSubCategory.map(item=>item.id)
+  const products = await Product.find({
+    product_category_id: { $in: [category.id, ...listSubCategoryId] },
+    deleted: false,
+  }).sort({ position: "desc" });
+  const productNew = productsHelper.priceNewProducts(products);
+  res.render("client/pages/products/index", {
+    pageTitle: category.title,
+    products: productNew,
+  });
+};
